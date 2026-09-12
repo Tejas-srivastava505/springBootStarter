a7c6d8cae8c6e6955fd059318e2e2fe3addbd127
Tejas <tejas.srivastava26@gmail.com>
2026-09-12 12:50:15 +0530

    API CustomStatusCode Del. not working properly

--- src/main/java/com/LearningRest/demo/Controller/JournalEntryControllerV2.java
+++ src/main/java/com/LearningRest/demo/Controller/JournalEntryControllerV2.java
@@ -5,6 +5,8 @@ import com.LearningRest.demo.Service.JournalEntryService;
 import com.LearningRest.demo.entity.JournalEntry;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -21,37 +23,58 @@ public class JournalEntryControllerV2 {
 
 
     @GetMapping
-    public ArrayList<JournalEntry> getAll(){
-        return new ArrayList<>(jes.getAll());
+    public ResponseEntity<ArrayList<JournalEntry>> getAll(){ //get ok or get NOT_FOUND.
+        try{
+            return new ResponseEntity<>(new ArrayList<>(jes.getAll()), HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @PostMapping
-    public JournalEntry addEntry(@RequestBody JournalEntry jy){
-        jy.setDate(LocalDateTime.now());
-        jes.saveEntry(jy);
-        return jy;
+    public ResponseEntity<JournalEntry> addEntry(@RequestBody JournalEntry jy){  //created or bad request.
+        try {
+            jy.setDate(LocalDateTime.now());
+            jes.saveEntry(jy);
+            return new ResponseEntity<>(jy,HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        }
     }
     //for Specific entry
-    @GetMapping("/user/{myID}")
-    public JournalEntry getByID(@PathVariable ObjectId myID){
-        return jes.getbyId(myID).orElse(null);
+    @GetMapping("/user/{myID}")  //either found or not_found
+    public ResponseEntity<JournalEntry> getByID(@PathVariable ObjectId myID){
+        JournalEntry found=jes.getbyId(myID).orElse(null);
+        if(found!=null){
+            return new ResponseEntity<>(found,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/user/{myId}")
-    public JournalEntry delById(@PathVariable ObjectId myId){
-        return jes.deleteById(myId);
+    @DeleteMapping("/user/{myId}") //Either deleted or not found
+    public ResponseEntity<JournalEntry> delById(@PathVariable ObjectId myId){
+        try{
+            jes.deleteById(myId);  //invalid request is also returning 204 No_Content
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/user/{myId}")
-    public JournalEntry updateById(@PathVariable ObjectId myId, @RequestBody JournalEntry je){
+    public ResponseEntity<JournalEntry> updateById(@PathVariable ObjectId myId, @RequestBody JournalEntry je){
         JournalEntry old =  jes.getbyId(myId).orElse(null); //get the object, if present.
         if(old == null){
-            return addEntry(je);
+            return new ResponseEntity<>(je,HttpStatus.NOT_FOUND);
         }
         //check if new is not null and empty
-        old.setTitle( ((!je.getTitle().isEmpty()) && (je.getTitle()!=null)) ? je.getTitle() : old.getTitle());
-        old.setContent( ((!je.getContent().isEmpty()) && (je.getContent()!=null)) ? je.getContent() : old.getContent());
+        old.setTitle( ((je.getTitle()!=null) && (!je.getTitle().isEmpty())) ? je.getTitle() : old.getTitle());
+        old.setContent( ((je.getContent()!=null) && (!je.getContent().isEmpty()) ) ? je.getContent() : old.getContent());
         jes.saveEntry(old);
-        return old;
+        return new ResponseEntity<>(old,HttpStatus.OK);
     }
 
 }
